9c7cf323e55388aa0ca34dd15a0dbf9432aac4b8
+ <+>
2024-01-12 12:42:20 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(nameArray, name){
+    return nameArray.filter(function(driver){
+        return driver.toUpperCase() === name.toUpperCase()
+    })
+}
+
+function fuzzyMatch(nameArray, letters){
+    return nameArray.filter(function(driver){
+        return driver.slice(0, letters.length) === letters
+    })
+}
+
+function matchName(driverArray, name){
+    return driverArray.filter(function(driver){
+        return driver.name === name
+    })
+}
